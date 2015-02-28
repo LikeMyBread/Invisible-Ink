10d8ad1fa51c3827deeb397df327f5c5a1e53f9f
shadowfiles <zhangtiff@gmail.com>
2015-02-28 03:10:01 -0500

    Restructured the code.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 window.onload = function (e) {
-    // Checking for allowed file upload scenarios
-    var file = document.getElementById('encode_file');
-    var canvas = document.getElementById('encode_image');
-    var upload = document.getElementById('encode_upload');
-    var ctx = canvas.getContext('2d');
-
-    var invertColors = function (e) {
-        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var invertColors = function (imageData, canvas, ctx) {
         var pixels = imageData.data;
         var numPixels = imageData.width * imageData.height;
         for (var i = 0; i < numPixels; i++) {
@@ -16,9 +9,58 @@ window.onload = function (e) {
         };
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.putImageData(imageData, 0, 0);
+    };
+
+    var menu = document.getElementById("main");
+    var encode_link = document.getElementById("encode_link");
+    var decode_link = document.getElementById("decode_link");
+
+    var encode = new Context("encode", "encode_error", "encode_image", "encode_file",
+        "encode_submit", invertColors);
+
+    var decode = new Context("decode", "decode_error", "decode_image", "decode_file",
+        "decode_submit", invertColors);
+
+    encode_link.addEventListener("click", function (event) {
+        menu.className = "side";
+        encode.show();
+        decode.hide();
+    }, false);
+
+    decode_link.addEventListener("click", function (event) {
+        menu.className = "side";
+        decode.show();
+        encode.hide();
+    }, false);
+    
+};
+
+function Context (id, error_id, canvas_id, file_id, submit_id, submit_callback) {
+    var container = document.getElementById(id);
+    var errorContainer = document.getElementById(error_id);
+    var extensions = ['jpg', 'jpeg', 'png', 'gif'];
+    var canvas = document.getElementById(canvas_id);
+    var file = document.getElementById(file_id);
+    var submit = document.getElementById(submit_id);
+    var ctx = canvas.getContext('2d');
+
+    file.addEventListener("change", function(event) {
+        var file = this.files[0];
+        checkFileValid(file);
+        handleImage(event);
+    }, false);
+
+    submit.addEventListener("click", function (event) {
+        submit_callback(getImageData(), canvas, ctx);
+    }, false);
+
+    this.show = function () {
+        container.style.display = "inline";
     }
 
-    upload.addEventListener("click", invertColors, false);
+    this.hide = function () {
+        container.style.display = "none";
+    }
 
     var getImageData = function () {
         return ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -38,32 +80,24 @@ window.onload = function (e) {
         reader.readAsDataURL(e.target.files[0]);
     }
 
-    file.addEventListener("change", function(event) {
-        var file = this.files[0];
-        checkFileValid(file);
-        handleImage(event);
-    }, false);
-};
+    var checkFileValid = function (file) {
+        var name = file.name;
+        var size = file.size;
+        var type =  file.type;
 
-var allowedExts = ['jpg', 'jpeg', 'png', 'gif'];
-
-var checkFileValid = function (file) {
-    var name = file.name;
-    var size = file.size;
-    var type =  file.type;
-
-    var error = "Invalid file type " + type + "! ";
-    var i = 0;
-    while (error != "" && i < allowedExts.length) {
-        var ext = 'image/' + allowedExts[i];
-        if (ext == type) {
-            error = "";
+        var error = "Invalid file type " + type + "! ";
+        var i = 0;
+        while (error != "" && i < extensions.length) {
+            var ext = 'image/' + extensions[i];
+            if (ext == type) {
+                error = "";
+            }
+            i++;
         }
-        i++;
+        showError(error);
     }
-    showError(error);
-}
 
-var showError = function (error) {
-    document.getElementById('encode_upload_error').innerHTML = error;
+    var showError = function (error) {
+        errorContainer.innerHTML = error;
+    }
 }
