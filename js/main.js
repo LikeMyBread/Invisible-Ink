08254538c5ed7720e18b4546493feca7c487d0df
shadowfiles <zhangtiff@gmail.com>
2015-03-01 05:38:25 -0500

    Prettifying.

--- js/main.js
+++ js/main.js
@@ -7,16 +7,30 @@ window.onload = function (e) {
 
     var decode = new DecodeContext();
 
-    encode_link.addEventListener("click", function (event) {
+    var select_encode = function (event) {
         menu.className = "side";
         encode.show();
         decode.hide();
-    }, false);
+        encode_link.className = "button selected";
+        decode_link.className = "button";
+    };
 
-    decode_link.addEventListener("click", function (event) {
+    var select_decode = function (event) {
         menu.className = "side";
         decode.show();
         encode.hide();
-    }, false);
+        decode_link.className = "button selected";
+        encode_link.className = "button";
+    };
+
+    if(window.location.hash == "#encode") {
+        select_encode();
+    } else if (window.location.hash == "#decode") {
+        select_decode();
+    }
+
+    encode_link.addEventListener("click", select_encode, false);
+
+    decode_link.addEventListener("click", select_decode, false);
     
 };
