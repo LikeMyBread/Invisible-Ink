aba14d6ee4be703f191b9217c6b12c60aa623177
shadowfiles <zhangtiff@gmail.com>
2015-02-28 23:02:17 -0500

    Refactoring.

---
+++ js/context.js
@@ -0,0 +1,137 @@
+function DecodeContext () {
+    that = new Context("decode", "decode_error", "decode_image", "decode_file",
+        "decode_submit", "decode_text");
+
+    var submit = that.getSubmit();
+    var textarea = that.getTextarea();
+
+    submit.addEventListener("click", decode_handler, false);
+
+    var decode_handler = function () {
+        var imageData = that.getImageData();
+        var text = parseImage(imageData.data);
+        textarea.value = text;
+        textarea.innerHTML = text;
+    };
+    return that;
+}
+
+function EncodeContext () {
+    that = new Context("encode", "encode_error", "encode_image", "encode_file",
+        "encode_submit", "encode_text");
+
+    var submit = that.getSubmit();
+    var textarea = that.getTextarea();
+    var canvas = that.getCanvas();
+    var ctx = that.getCtx();
+
+    var download = document.getElementById('download_encoded');
+
+    // Makes the download image link work. 
+    download.addEventListener('mouseover', function (e) {
+        var dataURL = canvas.toDataURL('image/png');
+        this.href = dataURL;
+    });
+
+    submit.addEventListener("click", encode_handler, false);
+
+    var encode_handler = function () {
+        var imageData = that.getImageData();
+        var text = textarea.value;
+        if (!text) {
+            text = textarea.innerHTML;
+        }
+
+        imageData.data = applyMessage(text, imageData.data);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.putImageData(imageData, 0, 0);
+    };
+    return that;
+}
+
+function Context (id, error_id, canvas_id, file_id, submit_id, text_id) {
+    var container = document.getElementById(id);
+    var errorContainer = document.getElementById(error_id);
+    var extensions = ['jpg', 'jpeg', 'png', 'gif'];
+    var canvas = document.getElementById(canvas_id);
+    var file = document.getElementById(file_id);
+    var submit = document.getElementById(submit_id);
+    var ctx = canvas.getContext('2d');
+    var textarea = document.getElementById(text_id);
+
+    this.getContainer = function () {
+        return container;
+    }
+
+    this.getCanvas = function () {
+        return canvas;
+    }
+
+    this.getCtx = function () {
+        return ctx;
+    }
+
+    this.getTextarea = function () {
+        return textarea;
+    }
+
+    this.getSubmit = function () {
+        return submit;
+    }
+
+    // Draws the image onto a canvas when it's picked
+    file.addEventListener("change", function(event) {
+        var file = this.files[0];
+        checkFileValid(file);
+        handleImage(event);
+    }, false);
+
+    this.show = function () {
+        container.style.display = "inline";
+    }
+
+    this.hide = function () {
+        container.style.display = "none";
+    }
+
+    this.getImageData = function () {
+        return ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
+
+    // Reads an image from a file upload
+    var handleImage = function (e) {
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var img = new Image();
+            img.onload = function(){
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+
+    var checkFileValid = function (file) {
+        var name = file.name;
+        var size = file.size;
+        var type =  file.type;
+
+        var error = "Invalid file type " + type + "! ";
+        var i = 0;
+        while (error != "" && i < extensions.length) {
+            var ext = 'image/' + extensions[i];
+            if (ext == type) {
+                error = "";
+            }
+            i++;
+        }
+        showError(error);
+    }
+
+    var showError = function (error) {
+        errorContainer.innerHTML = error;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
 window.onload = function (e) {
-    var download = document.getElementById('download_encoded');
-
-    var encode_text = document.getElementById("encode_text");
-    var decode_text = document.getElementById("decode_text");
-    var encode_handler = function (imageData, canvas, ctx) {
-        var text = encode_text.value;
-        if (!text) {
-            text = encode_text.innerHTML;
-        }
-        imageData.data = applyMessage(text, imageData.data);
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.putImageData(imageData, 0, 0);
-    };
-
-    var decode_handler = function (imageData, canvas, ctx) {
-        var text = parseImage(imageData.data);
-        decode_text.value = text;
-        decode_text.innerHTML = text;
-    };
-
     var menu = document.getElementById("main");
     var encode_link = document.getElementById("encode_link");
     var decode_link = document.getElementById("decode_link");
 
-    var encode = new Context("encode", "encode_error", "encode_image", "encode_file",
-        "encode_submit", encode_handler, "download_encoded", true);
+    var encode = new EncodeContext();
 
-    var decode = new Context("decode", "decode_error", "decode_image", "decode_file",
-        "decode_submit", decode_handler, false);
+    var decode = new DecodeContext();
 
     encode_link.addEventListener("click", function (event) {
         menu.className = "side";
@@ -43,79 +20,3 @@ window.onload = function (e) {
     }, false);
     
 };
-
-function Context (id, error_id, canvas_id, file_id, submit_id, submit_callback, download_id) {
-    var container = document.getElementById(id);
-    var errorContainer = document.getElementById(error_id);
-    var extensions = ['jpg', 'jpeg', 'png', 'gif'];
-    var canvas = document.getElementById(canvas_id);
-    var file = document.getElementById(file_id);
-    var submit = document.getElementById(submit_id);
-    var ctx = canvas.getContext('2d');
-
-    if (download_id) {
-        var download = document.getElementById(download_id);
-        download.addEventListener('mouseover', function (e) {
-            var dataURL = canvas.toDataURL('image/png');
-            this.href = dataURL;
-        });
-    }
-
-
-    file.addEventListener("change", function(event) {
-        var file = this.files[0];
-        checkFileValid(file);
-        handleImage(event);
-    }, false);
-
-    submit.addEventListener("click", function (event) {
-        submit_callback(getImageData(), canvas, ctx);
-    }, false);
-
-    this.show = function () {
-        container.style.display = "inline";
-    }
-
-    this.hide = function () {
-        container.style.display = "none";
-    }
-
-    var getImageData = function () {
-        return ctx.getImageData(0, 0, canvas.width, canvas.height);
-    }
-
-    var handleImage = function (e) {
-        var reader = new FileReader();
-        reader.onload = function(event){
-            var img = new Image();
-            img.onload = function(){
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
-            }
-            img.src = event.target.result;
-        }
-        reader.readAsDataURL(e.target.files[0]);
-    }
-
-    var checkFileValid = function (file) {
-        var name = file.name;
-        var size = file.size;
-        var type =  file.type;
-
-        var error = "Invalid file type " + type + "! ";
-        var i = 0;
-        while (error != "" && i < extensions.length) {
-            var ext = 'image/' + extensions[i];
-            if (ext == type) {
-                error = "";
-            }
-            i++;
-        }
-        showError(error);
-    }
-
-    var showError = function (error) {
-        errorContainer.innerHTML = error;
-    }
-}
